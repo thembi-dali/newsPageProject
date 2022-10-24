11793d031baad0849664e790c14b08f259de38ca
Thembi Baleni <thembi.baleni@gmail.com>
2022-10-24 16:25:43 +1100

    added html, css components and added js logic

--- force-app/main/default/lwc/newsComponent/newsComponent.js
+++ force-app/main/default/lwc/newsComponent/newsComponent.js
@@ -1,3 +1,46 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import retrieveNews from "@salesforce/apex/newsController.retrieveNews";
 
-export default class NewsComponent extends LightningElement {}
+export default class NewsComponent extends LightningElement {
+    @track result = []
+    @track selectedNews={};
+    @track isModalOpen =  false;
+
+    get modalClass(){
+        return `slds-modal ${this.isModalOpen ? "slds-fade-in-open" :""}`
+    }
+    get modalBackdropClass(){
+        return this.isModalOpen ? "slds-backdrop_open" : "slds_backdrop"
+    }
+    connectedCallback(){
+        this.fetchNews();
+    }
+    fetchNews(){
+        retrieveNews().then(response=>{
+            console.log(response);
+            this.formatNewsData(response.articles)
+        }).catch(error=>{
+            console.error(error);
+        })
+    }
+    formatNewsData(res){
+        this.result = res.map((item, index)=>{
+            let id = `new_${index+1}`;
+            let date = new Date(item.publishedAt).toDateString()
+            let name = item.source.name;
+            return { ...item, id: id, name: name, date: date}
+        })
+    }
+    showModal(event){
+        let id = event.target.dataset.item;
+        this.result.forEach(item=>{
+            if(item.id === id){
+                this.selectedNews ={...item}
+            }
+        })
+        this.isModalOpen = true;
+    }
+    closeModal(){
+        this.isModalOpen = false;
+    }
+}
